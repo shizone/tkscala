eff2fabce15c5381764836fe73b30f6a9e64529d
razon <shizonera@gmail.com>
2011-12-17 16:24:34 +0900

    #okajugで変更してます

--- source/conf.py
+++ source/conf.py
@@ -41,7 +41,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'tkscala'
-copyright = u'2010 - 2011 Tenryo Kurashiki Scala All Rights Reserved.'
+copyright = u'2010 - 2012 Tenryo Kurashiki Scala All Rights Reserved.'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
